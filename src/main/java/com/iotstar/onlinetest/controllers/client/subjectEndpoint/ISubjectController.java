cc9a470603b5378be9009bc104dd45564aaa424d
MDung0807 <dominhdung21082002@gmail.com>
2023-06-06 13:51:53 +0700

    update and non security

--- src/main/java/com/iotstar/onlinetest/controllers/client/subjectEndpoint/ISubjectController.java
+++ src/main/java/com/iotstar/onlinetest/controllers/client/subjectEndpoint/ISubjectController.java
@@ -5,13 +5,11 @@ import com.iotstar.onlinetest.DTOs.responses.Response;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/subject")
+@CrossOrigin
 public interface ISubjectController {
     @RequestMapping("/add")
     @PreAuthorize("hasRole(@environment.getProperty('ROLE_TEACHER'))")
